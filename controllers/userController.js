7bbc52c2ce8cff260ed3465fb7eb48e88557c5fb
Kartika <xiuminsbg@gmail.com>
2016-08-08 12:08:27 +0800

    updated controllers/userController and deleted views folder

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,60 @@
+
 module.exports = function (app, passport) {
   app.get('/', function (req, res) {
-    res.render('index.ejs'); // load the index.ejs file
+    res.status(201).json({message: 'Signup or login'});
   });
 
   app.get('/login', function (req, res) {
-    // render the page and pass in any flash data if it exists
-    res.render('login.ejs', {message: req.flash('loginMessage')});
+    res.status(201).json({message: 'Login'});
   });
 
   app.get('/signup', function (req, res) {
-    res.render('signup.ejs', { message: req.flash('signupMessage') });
-  });
-
-  app.get('/profile', isLoggedIn, function (req, res) {
-    res.render('profile.ejs', {
-      user: req.user
-    });
+    res.status(201).json({message: 'Signup'});
   });
 
   // Logout
   app.get('/logout', function (req, res) {
     req.logout();
-    res.redirect('/');
+    res.status(200).json({
+      status: 'Logged Out'
+    });
   });
 
   // signing up
+  app.get('/signupfailure', function (req, res) {
+    res.status(401).json({message: 'Signup Failed.', success: true});
+  });
+
+  app.get('/signupsuccess', function (req, res) {
+    res.status(200).json({message: 'Welcome! Signup successful!', success: true});
+  });
   app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/profile',
-    failureRedirect: '/signup',
+    successRedirect: '/signupsuccess',
+    failureRedirect: '/signupfailure',
     failureFlash: true
   }));
 
   // logging in
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect: '/profile',
-    failureRedirect: '/login',
-    failureFlash: true
-  }));
+  app.post('/login', function (req, res, next) {
+    passport.authenticate('local-login', function (err, user, info) {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(401).json({
+          err: info
+        });
+      }
+      req.logIn(user, function (err) {
+        if (err) {
+          return res.status(500).json({
+            err: 'Login denied.'
+          });
+        }
+        res.status(200).json({
+          status: 'Login successful!'
+        });
+      });
+    })(req, res, next);
+  });
 };
-
-function isLoggedIn (req, res, next) {
-  // if user is authenticated in the session, carry on
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-  }
-}
