5c38d72aa6d38f46fa600a4a1daf30333b4c68de
Kartika <xiuminsbg@gmail.com>
2016-08-11 07:50:39 +0800

    updated CORS: deleted OPTIONS method again

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const funfactController = require('./controllers/funfactController');
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE', 'OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
